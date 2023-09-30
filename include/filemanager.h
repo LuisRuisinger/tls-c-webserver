15f6a3396f9379309c71adcd31327fedc160f891
luisruisinger <luisruisinger.privat@gmail.com>
2023-09-30 17:36:24 +0200

    - added filemanager for opening and reading requested files

---
+++ include/filemanager.h
@@ -0,0 +1,8 @@
+//
+// Created by Luis Ruisinger on 30.09.23.
+//
+
+#ifndef WEBSERVER_C_FILEMANAGER_H
+#define WEBSERVER_C_FILEMANAGER_H
+
+#endif //WEBSERVER_C_FILEMANAGER_H
---
+++ src/filemanager.c
@@ -0,0 +1,32 @@
+//
+// Created by Luis Ruisinger on 30.09.23.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "../include/filemanager.h"
+#include "../include/serve.h"
+
+char* request_file (char* filename)
+{
+    char c;
+    FILE* file = fopen(filename, "r");
+
+    if (file == NULL)
+        return NULL;
+
+    fseek(file, 0, SEEK_END);
+    int64_t len = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    char* temp = malloc(sizeof(char) * (len + 1));
+
+    int32_t i = 0;
+    while((c = (char) fgetc(file)) != EOF)
+        temp[i++] = c;
+
+    fclose(file);
+    return temp;
+}
