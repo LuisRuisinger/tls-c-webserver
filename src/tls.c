66013b5e0147ddad521f785fcadbe8dc74de961e
EvilHedge <101642745+LuisRuisinger@users.noreply.github.com>
2023-10-02 14:34:47 +0200

    Create tls.c

---
+++ src/tls.c
@@ -0,0 +1,50 @@
+//
+// Created by Luis Ruisinger on 02.10.23.
+//
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#include "../include/tls.h"
+
+SSL_CTX* create_context()
+{
+    const SSL_METHOD *method;
+    SSL_CTX *ctx;
+
+    method = TLS_server_method();
+    ctx = SSL_CTX_new(method);
+
+    if (!ctx)
+    {
+        perror("Unable to create SSL context");
+        ERR_print_errors_fp(stderr);
+        return NULL;
+    }
+    return ctx;
+}
+
+void configure_context(SSL_CTX *ctx)
+{
+    if (SSL_CTX_use_certificate_file(ctx, "../certificates/cert.pem", SSL_FILETYPE_PEM) <= 0)
+    {
+        fprintf(stderr, "can't use or open cert.pem\n");
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    if (SSL_CTX_use_PrivateKey_file(ctx, "../certificates/key.pem", SSL_FILETYPE_PEM) <= 0 )
+    {
+        fprintf(stderr, "can't use or open key.pem\n");
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+}
