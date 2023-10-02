2264c814b9d5c3c22b14384c231fd2e556ad719f
EvilHedge <101642745+LuisRuisinger@users.noreply.github.com>
2023-10-02 14:36:03 +0200

    Update client.h

--- include/client.h
+++ include/client.h
@@ -6,11 +6,14 @@
 #define WEBSERVER_C_CLIENT_H
 
 #include <netinet/in.h>
+#include <openssl/ssl.h>
 
 typedef struct Client {
     int32_t fd;
     in_port_t sin6_port;
     struct in6_addr sin6_addr;
+    SSL_CTX* ctx;
+    SSL* ssl;
 } client;
 
 #endif //WEBSERVER_C_CLIENT_H
